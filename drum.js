c45d07c9389e0674ce3104930afdf207b9537867
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-13 20:20:16 -0300

    Inclui js

--- drum.js
+++ drum.js
@@ -1,3 +1,16 @@
-function selectMe(){
-    //determine if this is a single click, or a cntrol click 
-}
+addEventListener("keydown", function (event) {
+  if (event.keyCode == 86) document.body.style.background = "violet";
+});
+
+addEventListener("keyup", function (event) {
+  if (event.keyCode == 86) document.body.style.background = "";
+}); 
+
+addEventListener("keydown", function (event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  this.console.log(audio);
+  audio.play();
+  // const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  //if(!audio) return;
+  //audio.play();
+}); 
