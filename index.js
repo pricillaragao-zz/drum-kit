bf23c39245c60ed529f226b22117d1392ad283be
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-14 00:38:51 -0300

    inclui uma funcao de transicao para que as teclas se alternem entre si ao clicar uma sequena de teclas

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 addEventListener("keydown", function (event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+  key.classList.add("playing");
+  key.classList.remove("playing");
+  key.classList.toggle("playing");
 });
+
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
