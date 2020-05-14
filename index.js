300200b134f75ec6398af1bbc7cddb5278911c6c
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-13 21:57:12 -0300

    retorna o audio para o inicio quando o usuario clica novamente em uma tecla

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 addEventListener("keydown", function (event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   if (!audio) return;
-  this.console.log(audio);
+  audio.currentTime = 0;
   audio.play();
 });
